c27a44bbe2ae29335788e7e5c48d37286e99ba87
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-04 16:16:12 -0700

    Minor changes (untested)

--- graal_aotc_java/run.sh
+++ graal_aotc_java/run.sh
@@ -1,3 +1,6 @@
+set -e
+set -o pipefail
+
 NATIVE_IMAGE=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/lib/svm/bin/native-image
 
 test -f $NATIVE_IMAGE || echo "Does not exist: $NATIVE_IMAGE"
@@ -11,19 +14,16 @@ test -d $GRAALVM_HOME || exit 1
 
 $GRAALVM_HOME/bin/gu install native-image
 
-# JAR_WITH_DEPS=build/libs/graal_aotc_java.jar
-JAR_WITH_DEPS=build/libs/graal_aotc_java-all-1.0.jar
+./gradlew jar
+
 JAR_WITH_DEPS=build/libs/graal_aotc_java-1.0.jar
-#./target/graalvmnidemos-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 test -f $JAR_WITH_DEPS || echo "Does not exist: $JAR_WITH_DEPS"
 test -f $JAR_WITH_DEPS || exit 1
 
-
 # ./gradlew nativeBuild
 
-./gradlew jar
-java -jar /Volumes/git/github/docker_image_helloworld/graal_aotc_java/build/libs/graal_aotc_java-all-1.0.jar
+java -jar /Volumes/git/github/docker_image_helloworld/graal_aotc_java/build/libs/graal_aotc_java-1.0.jar
 
 # Note: this is case-sensitive
 $NATIVE_IMAGE -jar $JAR_WITH_DEPS --no-fallback --no-server -H:Class=com.Helloworld -H:Name=helloworld
