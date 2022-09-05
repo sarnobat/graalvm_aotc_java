259fe83b4b0867f6283495fdf6d1457a438b3317
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-05 13:06:35 -0700

    Not working

--- 4_java_transfer_url/run.sh
+++ 4_java_transfer_url/run.sh
@@ -2,5 +2,5 @@ set -e
 ls -d /Volumes/trash/trash/graalvm-ce-java11-22.2.0/
 ls -d /usr/local/Cellar/openjdk@11/11.0.12/
 /Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew nativeCompile -b app/build.gradle
+GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew nativeCompile -b app/build.gradle --debug
 ls -lh app/build/native/nativeCompile/app
