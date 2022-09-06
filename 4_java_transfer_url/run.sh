b5405923e982bae6d6611c048d5e1e626a0aae8e
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-05 20:51:41 -0700

    Minor changes (untested)

--- 4_java_transfer_url/run.sh
+++ 4_java_transfer_url/run.sh
@@ -4,3 +4,8 @@ ls -d /usr/local/Cellar/openjdk@11/11.0.12/
 /Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
 GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew clean nativeCompile -b app/build.gradle 
 ls -lh app/build/native/nativeCompile/app
+
+cat <<EOF
+# /Volumes/trash/trash/jersey/examples/helloworld-programmatic
+GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ mvn package -P native-image
+EOF
