ba301a1b68de1fcbfa1548d97f8257562ba56181
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-09 16:27:55 -0700

    Minor changes (untested)

--- 2_gradle_plugin/run_linux.sh
+++ 2_gradle_plugin/run_linux.sh
@@ -1,5 +1,13 @@
 set -e
-ls -d /Volumes/trash/trash/graalvm-ce-java11-22.2.0/
+GRAALVM_HOME=/home/sarnobat/trash/graalvm-ce-java11-22.2.0
+
+test -d $GRAALVM_HOME || echo "Does not exist: $GRAALVM_HOME"
+test -d $GRAALVM_HOME || exit 1
+
+$GRAALVM_HOME/bin/gu install native-image
+
+./gradlew jar
+
 ls -d /usr/local/Cellar/openjdk@11/11.0.12/
 /Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
 GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew nativeCompile -b app/build.gradle
