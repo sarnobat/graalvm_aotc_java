2863ee2e472450cfb1eff4e475c627e3ff51b8b9
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-09 16:24:14 -0700

    Minor changes (untested)

--- 5_java_httpcat_with_write/run.sh
+++ 5_java_httpcat_with_write/run.sh
@@ -1,11 +1,11 @@
 set -e
-ls -d /Volumes/trash/trash/graalvm-ce-java11-22.2.0/
-ls -d /usr/local/Cellar/openjdk@11/11.0.12/
-/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew clean nativeCompile -b app/build.gradle --info
+ls -d $HOME/trash/graalvm-ce-java11-22.2.0/
+ls -d /usr/lib/jvm/java-11-openjdk-amd64/
+$HOME/trash/graalvm-ce-java11-22.2.0//lib/installer/bin/gu  install native-image
+GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ ./gradlew clean nativeCompile -b app/build.gradle --debug
 ls -lh app/build/native/nativeCompile/app
 
 cat <<EOF
 # /Volumes/trash/trash/jersey/examples/helloworld-programmatic
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ mvn package -P native-image
+GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ mvn package -P native-image
 EOF
