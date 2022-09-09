0d65a58170654419d200d87995bb6e17c27be393
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-09 16:44:22 -0700

    Minor changes (untested)

--- 2_helloworld_gradle_plugin/run_linux.sh
+++ 2_helloworld_gradle_plugin/run_linux.sh
@@ -8,7 +8,10 @@ $GRAALVM_HOME/bin/gu install native-image
 
 ./gradlew jar
 
-ls -d /usr/local/Cellar/openjdk@11/11.0.12/
-/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew nativeCompile -b app/build.gradle
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+ls -d $JAVA_HOME
+
+$GRAALVM_HOME/lib/installer/bin/gu  install native-image
+echo "Use -R:MaxHeapSize=14g , not -J-Xmx14g"
+GRAALVM_HOME=/home/sarnobat/trash/graalvm-ce-java11-22.2.0 JAVA_HOME=/home/sarnobat/trash/graalvm-ce-java11-22.2.0 ./gradlew nativeCompile -b app/build.gradle
 ls -lh app/build/native/nativeCompile/app
