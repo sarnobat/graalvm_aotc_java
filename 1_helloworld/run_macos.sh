98c3ce55fc476cc9876c96870a8d96532a6ed6b2
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-09 20:51:31 -0700

    Minor changes (untested)

--- 1_helloworld/run_macos.sh
+++ 1_helloworld/run_macos.sh
@@ -3,18 +3,20 @@
 set -e
 #set -o pipefail
 
-NATIVE_IMAGE=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/lib/svm/bin/native-image
-
-test -f $NATIVE_IMAGE || echo "Does not exist: $NATIVE_IMAGE"
-test -f $NATIVE_IMAGE || exit 1
-
 
 GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/
 
 test -d $GRAALVM_HOME || echo "Does not exist: $GRAALVM_HOME"
 test -d $GRAALVM_HOME || exit 1
 
+# Downloads "native-image"
 $GRAALVM_HOME/bin/gu install native-image
+
+NATIVE_IMAGE=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/lib/svm/bin/native-image
+
+test -f $NATIVE_IMAGE || echo "Does not exist: $NATIVE_IMAGE"
+test -f $NATIVE_IMAGE || exit 1
+
 
 ./gradlew jar
 
@@ -28,6 +30,6 @@ test -f $JAR_WITH_DEPS || exit 1
 java -jar /Volumes/git/github/docker_image_helloworld/graal_aotc_java/build/libs/graal_aotc_java-1.0.jar
 
 # Note: this is case-sensitive
-$NATIVE_IMAGE -jar $JAR_WITH_DEPS --no-fallback --no-server -H:Class=com.Helloworld -H:Name=helloworld
+$NATIVE_IMAGE -jar $JAR_WITH_DEPS --no-fallback --no-server -H:Class=com.Helloworld -H:Name=helloworld.macos
 
-./helloworld
+./helloworld.macos
