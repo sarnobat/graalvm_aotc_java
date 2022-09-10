94ab05b10a5cc8938b050591c9b764dcab4c12d6
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-09 19:26:31 -0700

    Get working native image on linux

--- 5_java_httpcat_with_write/run.sh
+++ 5_java_httpcat_with_write/run.sh
@@ -2,7 +2,7 @@ set -e
 ls -d $HOME/trash/graalvm-ce-java11-22.2.0/
 ls -d /usr/lib/jvm/java-11-openjdk-amd64/
 $HOME/trash/graalvm-ce-java11-22.2.0//lib/installer/bin/gu  install native-image
-GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ ./gradlew clean nativeCompile -b app/build.gradle --debug
+GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ ./gradlew clean nativeCompile -b app/build.gradle
 ls -lh app/build/native/nativeCompile/app
 
 cat <<EOF
