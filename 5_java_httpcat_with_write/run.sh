358092752324cb83587516f1e7660b6949a9519a
Sridhar Sarnobat <ss401533@gmail.com>
2022-09-09 19:27:24 -0700

    Minor changes (untested)

--- 5_java_httpcat_with_write/run.sh
+++ 5_java_httpcat_with_write/run.sh
@@ -2,6 +2,9 @@ set -e
 ls -d $HOME/trash/graalvm-ce-java11-22.2.0/
 ls -d /usr/lib/jvm/java-11-openjdk-amd64/
 $HOME/trash/graalvm-ce-java11-22.2.0//lib/installer/bin/gu  install native-image
+cat <<EOF
+!!!! If the next part runs out of memory, do killall java and check free -h !!!!
+EOF
 GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ ./gradlew clean nativeCompile -b app/build.gradle
 ls -lh app/build/native/nativeCompile/app
 
