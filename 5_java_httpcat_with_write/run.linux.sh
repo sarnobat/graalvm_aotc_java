4eae92afa82053dd659ea82b3bef764cf4a79eb5
Sridhar Sarnobat <ss401533@gmail.com>
2022-11-13 23:18:51 -0800

    Minor changes (untested)

--- 5_java_httpcat_with_write/run.linux.sh
+++ 5_java_httpcat_with_write/run.linux.sh
@@ -9,6 +9,9 @@ GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm
 ls -lh app/build/native/nativeCompile/app
 
 cat <<EOF
+/home/sarnobat/trash/graalvm-ce-java11-22.2.0/bin/native-image -jar app/build/libs/app.jar  --no-fallback --no-server -H:Class=com.HttpCatWithWrite        -H:Name=httpcatwithwrite --allow-incomplete-classpath
+EOF
+cat <<EOF >/dev/null
 # /Volumes/trash/trash/jersey/examples/helloworld-programmatic
 GRAALVM_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ JAVA_HOME=$HOME/trash/graalvm-ce-java11-22.2.0/ mvn package -P native-image
 EOF
