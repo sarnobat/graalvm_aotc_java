2a34d4eb548964afd1be86ffa76a2dbcf8001d66
Sridhar Sarnobat <ss401533@gmail.com>
2023-01-26 20:50:38 -0800

    Minor changes (untested)

--- 7_bash_parser/run_macos.sh
+++ 7_bash_parser/run_macos.sh
@@ -1,6 +1,7 @@
 set -e
-ls -d /Volumes/trash/trash/graalvm-ce-java11-22.2.0/
+GRAAL=/Volumes/Apps/graalvm-ce-java11-22.2.0
+ls -d $GRAAL
 ls -d /usr/local/Cellar/openjdk@11/11.0.12/
-/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew nativeCompile -b app/build.gradle
+$GRAAL/Contents/Home/lib/installer/bin/gu  install native-image
+GRAALVM_HOME=$GRAAL/Contents/Home/ JAVA_HOME=$GRAAL/Contents/Home/ ./gradlew nativeCompile -b app/build.gradle
 ls -lh app/build/native/nativeCompile/app
