9cf20dcda6d69964b5c3d84581c5e7e335975ba7
Sridhar Sarnobat <ss401533@gmail.com>
2023-01-31 21:13:28 -0800

    Minor changes (untested)

--- 7_bash_parser/run_macos.sh
+++ 7_bash_parser/run_macos.sh
@@ -5,3 +5,4 @@ ls -d /usr/local/Cellar/openjdk@11/11.0.12/
 $GRAAL/Contents/Home/lib/installer/bin/gu  install native-image
 GRAALVM_HOME=$GRAAL/Contents/Home/ JAVA_HOME=$GRAAL/Contents/Home/ ./gradlew nativeCompile -b app/build.gradle
 ls -lh app/build/native/nativeCompile/app
+cp app/build/native/nativeCompile/app bashparser.osx
