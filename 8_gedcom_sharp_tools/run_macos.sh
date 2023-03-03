6fd63670f3a324020e92a2924560bc39c48c4f01
Sridhar Sarnobat <ss401533@gmail.com>
2023-03-03 15:02:21 +0530

    Minor changes (untested)

--- 8_gedcom_sharp_tools/run_macos.sh
+++ 8_gedcom_sharp_tools/run_macos.sh
@@ -9,3 +9,6 @@ GRAALVM_HOME=$GRAAL/Contents/Home/ JAVA_HOME=$GRAAL/Contents/Home/ ./gradlew nat
 ls -lh app/build/native/nativeCompile/app
 # cp -v app/build/native/nativeCompile/app gedcom.osx
 cp -v app/build/native/nativeCompile/app gedcom2mwk.osx
+cat <<EOF | batcat --plain --paging=never --language sh --theme TwoDark
+./gedcom2mwk.osx ~/sarnobat.git/genealogy/rohidekar.ged | tee ~/sarnobat.git/genealogy/rohidekar.auto.`date -I`.mwk | pandoc --from mediawiki --to html | tee ~/sarnobat.git/genealogy/rohidekar.auto.`date -I`.mwk | xq | tee ~/sarnobat.git/genealogy/rohidekar.auto.`date -I`.html
+EOF
