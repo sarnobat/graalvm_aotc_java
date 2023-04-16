d806ae52745dfe74b043c35bad1214107e87557f
Sridhar Sarnobat <srsarnob@cisco.com>
2023-04-15 22:09:35 -0700

    Minor changes (untested)

---
+++ 3_java_callgraph/run_m1.sh
@@ -0,0 +1,12 @@
+set -e
+GRAALVM_HOME=/Volumes/trash/trash/2022/graalvm-ce-java11-22.2.0/
+# JDK_HOME=/Volumes/apps/homebrew/Cellar/openjdk@11/11.0.15/
+JDK_HOME=$GRAALVM_HOME
+ls -d $GRAALVM_HOME
+ls -d $JDK_HOME
+$GRAALVM_HOME/Contents/Home/lib/installer/bin/gu  install native-image
+GRAALVM_HOME=$GRAALVM_HOME/Contents/Home/ JAVA_HOME=$JDK_HOME/Contents/Home ./gradlew nativeCompile -b app/build.gradle
+#ls -lh app/build/native/nativeCompile/app
+mv /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/app/build/native/nativeCompile/app /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/java_callgraph.m1
+/Volumes/git/github/graalvm_aotc_java/3_java_callgraph/app/build/native/nativeCompile/app /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/
+
