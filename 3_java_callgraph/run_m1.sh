f7f46f11f3d11e9f71cc01a09c5ff0f0e60f6eae
Sridhar Sarnobat <srsarnob@cisco.com>
2023-04-17 12:03:04 -0700

    Cron daily autocommit

--- 3_java_callgraph/run_m1.sh
+++ 3_java_callgraph/run_m1.sh
@@ -8,4 +8,6 @@ $GRAALVM_HOME/Contents/Home/lib/installer/bin/gu  install native-image
 GRAALVM_HOME=$GRAALVM_HOME/Contents/Home/ JAVA_HOME=$JDK_HOME/Contents/Home ./gradlew nativeCompile -b app/build.gradle
 #ls -lh app/build/native/nativeCompile/app
 mv /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/app/build/native/nativeCompile/app /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/java_callgraph.m1
-/Volumes/git/github/graalvm_aotc_java/3_java_callgraph/java_callgraph.m1 ~/jd/module/ | tee /tmp/calls.csv
+# /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/java_callgraph.m1 ~/jd/module/ | tee /tmp/calls.csv
+find ~/jd/module/ -maxdepth 50 -type f -iname "**class" | grep -i -e jwt -e micro | /Volumes/git/github/graalvm_aotc_java/3_java_callgraph/java_callgraph.m1 | tee /tmp/calls.csv
+echo "now execute:\nsh ~/github/d3_csv/singlefile_automated/run_automated.sh | tee /tmp/index.html"
