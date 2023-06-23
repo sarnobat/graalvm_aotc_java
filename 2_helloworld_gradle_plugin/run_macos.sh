ce86df61a44792b0288523a3a8cbe2c56dcdec47
Sridhar Sarnobat <srsarnob@cisco.com>
2023-06-23 13:46:38 -0700

    Minor changes (untested)

--- 2_helloworld_gradle_plugin/run_macos.sh
+++ 2_helloworld_gradle_plugin/run_macos.sh
@@ -1,6 +1,8 @@
 set -e
-ls -d /Volumes/trash/trash/graalvm-ce-java11-22.2.0/
-ls -d /usr/local/Cellar/openjdk@11/11.0.12/
-/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/lib/installer/bin/gu  install native-image
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-ce-java11-22.2.0/Contents/Home/ JAVA_HOME=/usr/local/Cellar/openjdk@11/11.0.12/ ./gradlew nativeCompile -b app/build.gradle
+GRAALVM_DIST="/Volumes/trash/trash/graalvm-ce-java11-22.2.0/"
+JDK_DIST="/usr/local/Cellar/openjdk@11/11.0.12/"
+test -d "${GRAALVM_DIST}"
+test -d "${JDK_DIST}"
+"${GRAALVM_DIST}"/Contents/Home/lib/installer/bin/gu  install native-image
+GRAALVM_HOME="${GRAALVM_DIST}"Contents/Home/ JAVA_HOME="$JDK_DIST" ./gradlew nativeCompile -b app/build.gradle
 ls -lh app/build/native/nativeCompile/app
