29fd1ad4e7f69e939d57fd94579a402ef35c6b67
Sridhar Sarnobat.deprecated <sarnobat@air.local>
2024-04-12 18:31:01 -0700

    Minor changes (air)

--- 1_helloworld/run_macos.sh
+++ 1_helloworld/run_macos.sh
@@ -2,40 +2,48 @@ set -e
 #set -o pipefail
 
 # GRAALVM_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/
-GRAALVM_HOME=/Volumes/trash/trash/graalvm-jdk-17.0.7+8.1
+# GRAALVM_HOME=/Volumes/trash/trash/graalvm-jdk-17.0.7+8.1
+GRAALVM_HOME=/Volumes/Apps/graalvm-ce-java11-22.2.0/Contents/Home/
 
-test -d $GRAALVM_HOME || echo "Does not exist: $GRAALVM_HOME"
+test -d $GRAALVM_HOME || echo "4 Does not exist: $GRAALVM_HOME"
 test -d $GRAALVM_HOME || exit 1
 
-cat <<EOF | batcat --plain --paging=never --language sh --theme TwoDark
+NATIVE_IMAGE=$GRAALVM_HOME/lib/svm/bin/native-image
+JAR_WITH_DEPS=build/libs/graal_aotc_java-1.0.jar
+
+cat <<EOF | tee /tmp/compile_graalvm.sh | batcat --plain --paging=never --language sh --theme TwoDark
+set -e
+
 ## This doesn't use a gradle plugin for native compilation, see next code sample
-
-
-
-
 
 # Downloads "native-image"
 $GRAALVM_HOME/bin/gu install native-image
 
-NATIVE_IMAGE=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home/lib/svm/bin/native-image
 
-test -f $NATIVE_IMAGE || echo "Does not exist: $NATIVE_IMAGE"
+test -z $NATIVE_IMAGE && echo "1 Does not exist: $NATIVE_IMAGE"
+test -f $NATIVE_IMAGE || echo "2 Does not exist: $NATIVE_IMAGE"
 test -f $NATIVE_IMAGE || exit 1
+test -z $NATIVE_IMAGE && exit 1
 
 
 ./gradlew jar
 
-JAR_WITH_DEPS=build/libs/graal_aotc_java-1.0.jar
 
-test -f $JAR_WITH_DEPS || echo "Does not exist: $JAR_WITH_DEPS"
+
+test -f $JAR_WITH_DEPS || echo "3 Does not exist: $JAR_WITH_DEPS"
 test -f $JAR_WITH_DEPS || exit 1
 
 # ./gradlew nativeBuild
 
-java -jar /Volumes/git/github/docker_image_helloworld/graal_aotc_java/build/libs/graal_aotc_java-1.0.jar
+java -jar ./build/libs/graal_aotc_java-1.0.jar
 
 # Note: this is case-sensitive
 $NATIVE_IMAGE -jar $JAR_WITH_DEPS --no-fallback --no-server -H:Class=com.Helloworld -H:Name=helloworld.macos
 
 ./helloworld.macos
+EOF
+
+cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
+
+sh /tmp/compile_graalvm.sh
 EOF
